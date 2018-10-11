a9ba24e573522067b199d675973378051b38dc00
Sam Serrels <s.serrels@napier.ac.uk>
2018-10-11 14:16:20 +0100

    https

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 mkdir -p /build && cd /build
 export CC=/usr/bin/clang-6.0 && export CXX=/usr/bin/clang++-6.0
-/cmake/bin/cmake /src -DGPUVIS_build_tests=ON
-/cmake/bin/cmake --build . --config Release
-cd /src/webserver && npm install
+/cmake/bin/cmake /src -DGPUVIS_build_tests=ON && \
+/cmake/bin/cmake --build . --config Release && \
+cd /src/webserver && npm install && \
 cd /
